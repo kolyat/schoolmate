ffa24c70a9febf6c133ac7ab5c5826b1f387b304
kolyat <kks.pub@gmail.com>
2018-10-01 00:19:00 +0300

    Update testutils

--- testutils/rndutils.py
+++ testutils/rndutils.py
@@ -1,8 +1,18 @@
 # Copyright (c) 2017-2018 Kirill 'Kolyat' Kiselnikov
 # This file is the part of testutils, released under modified MIT license
-# See the file LICENSE included in this distribution
 
 import random
+import itertools
+import mimesis
+import mimesis.builtins
+from mimesis import enums
+
+from schoolmate import settings
+
+
+FORM_NUMBERS = [str(n) for n in range(1, 12)]
+FORM_LETTERS = 'АБВ'
+FORMS = [''.join(f) for f in itertools.product(FORM_NUMBERS, FORM_LETTERS)]
 
 
 def random_char():
@@ -42,3 +52,30 @@ def random_id():
     """
     random.seed()
     return random.randint(100000, 999999)
+
+
+def new_schooluser():
+    """Generate data for fake school user
+
+    :return: dict with person data
+    """
+    random.seed()
+    _person = mimesis.Person(locale='ru')
+    _person_ru = mimesis.builtins.RussiaSpecProvider()
+    _date = mimesis.Datetime()
+    _gender = random.choice((enums.Gender.MALE, enums.Gender.FEMALE))
+    _username = _person.username('ld')
+    return {
+        'username': _username,
+        'password': _username,
+        'first_name': _person.name(_gender),
+        'last_name': _person.surname(_gender),
+        'patronymic_name': _person_ru.patronymic(_gender),
+        'birth_date': _date.date(start=1990, end=2000, fmt='%Y-%m-%d'),
+        'email': _person.email(),
+        'school_form': random.choice(FORMS),
+        'language': random.choice(settings.LANGUAGES)[0],
+        'is_active': True,
+        'is_staff': False,
+        'is_superuser': False
+    }
--- testutils/settings.py
+++ testutils/settings.py
@@ -5,7 +5,8 @@ import logging
 #
 LOG_OPTIONS = {
     'filemode': 'w',
-    'format': '%(asctime)s [%(module)15s] %(levelname)7s - %(funcName)s - %(message)s',
+    'format': '%(asctime)s [%(module)15s] %(levelname)7s - %(funcName)s'
+              ' - %(message)s',
     'level': logging.INFO
 }
 
