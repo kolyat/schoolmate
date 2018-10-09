ae3e4113cea88c986ca2afcb6eac1e0830d2c775
Kolyat <kks.pub@gmail.com>
2018-10-09 03:02:49 +0300

    Update locales

--- runbabel.py
+++ runbabel.py
@@ -5,9 +5,12 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Babel launcher')
-    parser.add_argument('-c', '--create', action='store_true',
+    parser.add_argument('-e', '--extract', action='store_true',
                         default=False, required=False,
-                        help='create locale files', dest='create')
+                        help='get translation strings', dest='extract')
+    parser.add_argument('-i', '--init', action='store_true',
+                        default=False, required=False,
+                        help='create/overwrite locale files', dest='init')
     parser.add_argument('-u', '--update', action='store_true',
                         default=False, required=False,
                         help='update locale files', dest='update')
@@ -16,27 +19,44 @@ if __name__ == '__main__':
                         help='compile locale files', dest='compile')
     args = parser.parse_args()
 
-    if args.create:
+    opts = ' --msgid-bugs-address=kks.pub@gmail.com ' \
+           ' --copyright-holder="Kirill \'Kolyat\' Kiselnikov" ' \
+           ' --project=Schoolmate ' \
+           ' --version=0.1 '
+
+    if args.extract:
         os.system('pybabel extract -F babelcfg/babel.common.cfg'
-                  ' -o locale/django.pot .')
+                  ' -o locale/django.pot {} .'.format(opts))
+        os.system('pybabel extract -F babelcfg/babel.school.cfg'
+                  ' -o school/locale/django.pot {} .'.format(opts))
+        os.system('pybabel extract -F babelcfg/babel.account.cfg'
+                  ' -o account/locale/django.pot {} .'.format(opts))
+
+    if args.init:
         os.system('pybabel init -D django -i locale/django.pot'
                   ' -d locale -l ru')
         os.system('pybabel init -D django -i locale/django.pot'
                   ' -d locale -l de')
-        os.system('pybabel extract -F babelcfg/babel.account.cfg'
-                  ' -o account/locale/django.pot .')
+        os.system('pybabel init -D django -i school/locale/django.pot'
+                  ' -d school/locale -l ru')
+        os.system('pybabel init -D django -i school/locale/django.pot'
+                  ' -d school/locale -l de')
         os.system('pybabel init -D django -i account/locale/django.pot'
                   ' -d account/locale -l ru')
         os.system('pybabel init -D django -i account/locale/django.pot'
                   ' -d account/locale -l de')
 
     if args.update:
-        os.system('pybabel update -D django -i locale/django.pot -d locale')
+        os.system('pybabel update -D django -i locale/django.pot -d locale'
+                  ' --previous')
+        os.system('pybabel update -D django -i school/locale/django.pot'
+                  ' -d school/locale --previous')
         os.system('pybabel update -D django -i account/locale/django.pot'
-                  ' -d account/locale')
+                  ' -d account/locale --previous')
 
     if args.compile:
         os.system('pybabel compile -D django -d locale')
+        os.system('pybabel compile -D django -d school/locale')
         os.system('pybabel compile -D django -d account/locale')
 
     if len(sys.argv) < 2:
