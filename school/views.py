c4405d2d31d6c4442a226729fee9b2b080f2a3ee
Kolyat <kks.pub@gmail.com>
2018-11-01 00:04:29 +0300

    Extend status view

--- school/views.py
+++ school/views.py
@@ -18,37 +18,47 @@ from django.contrib.auth import decorators as auth_decorators
 from django.utils.decorators import method_decorator
 from django.utils import timezone
 
-from rest_framework import serializers, response, status
-from rest_framework import views
+from rest_framework import views, serializers, response, status
 
 from . import models
 
 
-class StatusSerializer(serializers.ModelSerializer):
+class DailyScheduleSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.DailySchedule
         fields = ('description',)
 
 
+class YearScheduleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.YearSchedule
+        fields = ('period_type', 'description')
+
+
 class Status(views.APIView):
-    model = models.DailySchedule
-
-    def get_object(self, time):
-        try:
-            return self.model.objects.get(start_time__lte=time,
-                                          end_time__gte=time)
-        except self.model.DoesNotExist:
-            self.model(description='')
-
     @method_decorator(auth_decorators.login_required)
     def get(self, request):
-        """:return: server's time with period description
+        """:return: server's date and time with period description
         """
         now = timezone.localtime(timezone.now())
-        _time = now.timetz()
-        schedule_obj = self.get_object(_time)
-        serializer = StatusSerializer(schedule_obj, many=False)
-        status_info = serializer.data
-        status_info.update({'hour': _time.hour, 'minute': _time.minute,
-                            'second': _time.second})
+        date = now.date()
+        time = now.timetz()
+        status_info = dict()
+        ys_obj = models.YearSchedule.objects.filter(
+            start_date__lte=date, end_date__gte=date).order_by('-period_type')
+        ys_serializer = YearScheduleSerializer(ys_obj, many=True)
+        status_info.update({'date_description': ys_serializer.data})
+        for ys in ys_serializer.data:
+            if ys['period_type'] == 'Q':
+                ds_obj = models.DailySchedule.objects.filter(
+                        start_time__lte=time, end_time__gte=time)
+                ds_serializer = DailyScheduleSerializer(ds_obj, many=True)
+                status_info.update({'time_description': ds_serializer.data})
+                break
+        else:
+            status_info.update({'time_description': list()})
+        status_info.update({
+            'year': date.year, 'month': date.month, 'day': date.day,
+            'hour': time.hour, 'minute': time.minute, 'second': time.second
+        })
         return response.Response(status_info, status=status.HTTP_200_OK)
