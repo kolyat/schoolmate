66e25d73046d119f6bce462f7cd5e7d046527636
Kolyat <kks.pub@gmail.com>
2018-11-16 01:34:54 +0300

    Dynamic column update

--- timetable/static/timetable/js/timetable.js
+++ timetable/static/timetable/js/timetable.js
@@ -17,20 +17,28 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 var formTree = {view: "tree", id: "form_tree", width: 125};
-var timeTable = {view: "datatable", id: "timetable"};
+var timeTable = {
+    view: "datatable", id: "timetable", columns: [
+        {id: "root", header: "", fillspace: true}
+    ]
+};
 webix.ui({
     id: "timetable_layout", type: "space", padding: 0,
-    rows: [{cols: [
-        formTree,
-        {view: "resizer"},
-        timeTable,
-    ]}]
+    rows: [{cols: [formTree, {view: "resizer"}, timeTable]}]
 });
 var form_tree = $$("form_tree");
 var timetable = $$("timetable");
 
 
-var school_forms;
+var DAYS_OF_WEEK = {
+    2: gettext("Monday"),
+    3: gettext("Tuesday"),
+    4: gettext("Wednesday"),
+    5: gettext("Thursday"),
+    6: gettext("Friday"),
+    7: gettext("Saturday")
+};
+var school_forms = new Map();
 
 function getTimetable(number) {
     if (number > 0) {
@@ -38,10 +46,27 @@ function getTimetable(number) {
     } else {
         params = {};
     }
+    timetable.config.columns.splice(1, timetable.config.columns.length - 1);
+    timetable.config.columns[0].fillspace = true;
+    timetable.refreshColumns();
+    timetable.refresh();
+    timetable.hideOverlay();
+    timetable.showOverlay(gettext("Loading..."));
     var promise = webix.ajax().get("/timetable/data/", params);
     promise.then(function(data) {
-        // TODO: write logic
+        var tt = data.json();
+        timetable.config.columns[0].fillspace = false;
+        timetable.config.columns[0].width = 90;
+        tt.forEach(function(form) {
+            var f = [form.form_number.toString(), form.form_letter].join("");
+            timetable.config.columns.push({id: f, header: f});
+        });
+        timetable.refreshColumns();
+        timetable.refresh();
+        timetable.hideOverlay();
     }).fail(function(err) {
+        timetable.hideOverlay();
+        timetable.showOverlay(gettext("No data"));
         webix.message({
             text: gettext("Unable to retrieve timetable"),
             type: "error",
@@ -54,10 +79,11 @@ function getTimetable(number) {
 function getForms() {
     var promise = webix.ajax().get("/main/forms/");
     promise.then(function(data) {
-        school_forms = data.json();
+        var _forms = data.json();
         form_tree.add({id: "form_numbers", value: gettext("Forms")});
         form_tree.add({id: "0", value: gettext("All")}, -1, "form_numbers");
-        school_forms.forEach(function(el) {
+        _forms.forEach(function(el) {
+            school_forms.set(el["number"], el["letters"]);
             form_tree.add(
                 {id: el["number"].toString(), value: el["number"].toString()},
                 -1, "form_numbers"
@@ -81,3 +107,4 @@ form_tree.attachEvent("onItemClick", function(id) {
         getTimetable(_number);
     }
 });
+timetable.showOverlay(gettext("Select form to load timetable"));
