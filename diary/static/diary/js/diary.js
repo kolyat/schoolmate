df37233f9e24f3b550ff55667a8394a830461968
kolyat <kks.pub@gmail.com>
2019-12-31 12:23:40 +0300

    Initial toolbar

--- diary/static/diary/js/diary.js
+++ diary/static/diary/js/diary.js
@@ -17,15 +17,38 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 var layoutMargin = 5;
+var navBar = {
+    view: "toolbar", id: "navbar", cols: [
+        {},
+        {
+            view: "button", id: "prev_button", name: "prev_button",
+            value: gettext("Previous"), label: "",
+            type: "icon", css: "fas fa-angle-double-left",
+            minWidth: 40, width: 50, minHeight: 30, height: 30, align: "right",
+            click: function() {} // TODO: implement
+        },
+        {
+            view: "datepicker", id: "current_date", stringResult: false,
+            value: new Date(), icons: false, // TODO: define icon
+            invalid: true, invalidMessage: gettext("Invalid date"),
+            minWidth: 80, width: 120, minHeight: 30, height: 30, align: "center"
+        },
+        {
+            view: "button", id: "next_button", name: "next_button",
+            value: gettext("Next"), label: "",
+            type: "icon", css: "fas fa-angle-double-right",
+            minWidth: 40, width: 50, minHeight: 30, height: 30, align: "left",
+            click: function() {} // TODO: implement
+        },
+        {}
+    ],
+    minHeight: 40, height: 40
+};
 webix.ui({
     type: "space", paddingY: 30,
     rows: [
         {
-            id: "toolbar_layout", margin: layoutMargin, cols: [
-                {
-                    // TODO: add toolbar elements
-                }
-            ]
+            id: "navbar_layout", margin: layoutMargin, cols: [navBar]
         },
         {
             id: "records_layout", margin: layoutMargin, cols: [
