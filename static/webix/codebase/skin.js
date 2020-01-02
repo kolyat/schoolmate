081e18f1c18ff1e033a17915f2094d9651125b04
kolyat <kks.pub@gmail.com>
2020-01-02 16:03:08 +0300

    Upgrade Webix from 5.0 to 7.1

--- static/webix/codebase/skin.js
+++ static/webix/codebase/skin.js
@@ -1,3 +1,3 @@
 //[Skin Customization]
-webix.skin.contrast.barHeight=46;webix.skin.contrast.tabbarHeight=46;webix.skin.contrast.rowHeight=34;webix.skin.contrast.listItemHeight=34;webix.skin.contrast.inputHeight=38;webix.skin.contrast.layoutMargin.wide=10;webix.skin.contrast.layoutMargin.space=10;webix.skin.contrast.layoutPadding.space=10;
+webix.skin.contrast.barHeight=42;webix.skin.contrast.tabbarHeight=40;webix.skin.contrast.rowHeight=32;webix.skin.contrast.listItemHeight=32;webix.skin.contrast.inputHeight=32;webix.skin.contrast.layoutMargin.wide=10;webix.skin.contrast.layoutMargin.space=10;webix.skin.contrast.layoutPadding.space=10;
  webix.skin.set('contrast');
