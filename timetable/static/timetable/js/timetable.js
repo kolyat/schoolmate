af03915cba1f2c810e1dcbd786b46470f358c060
kolyat <kks.pub@gmail.com>
2020-01-28 13:37:39 +0300

    Minor refactoring

--- timetable/static/timetable/js/timetable.js
+++ timetable/static/timetable/js/timetable.js
@@ -16,6 +16,15 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+//
+// General
+//
+var URL_SCHOOL_FORMS = "/main/forms/";
+var URL_TIMETABLE_DATA = "/timetable/data/";
+
+//
+// Widget description and init
+//
 var formTree = {view: "tree", id: "form_tree", width: 125};
 var timeTable = {
     view: "datatable", id: "timetable", fixedRowHeight: false,
@@ -23,14 +32,18 @@ var timeTable = {
         {id: "root", header: "", fillspace: true}
     ]
 };
+
 webix.ui({
     id: "timetable_layout", type: "space", padding: 0,
     rows: [{cols: [formTree, {view: "resizer"}, timeTable]}, {gravity: 0.08}]
 });
+
 var form_tree = $$("form_tree");
 var timetable = $$("timetable");
 
-
+//
+// UI logic
+//
 var DAYS_OF_WEEK = new Map([
     [2, gettext("Monday")],
     [3, gettext("Tuesday")],
@@ -55,9 +68,9 @@ function getTimetable(number) {
     } else {
         params = {};
     }
-    var promise = webix.ajax().get("/timetable/data/", params);
+    var promise = webix.ajax().get(URL_TIMETABLE_DATA, params);
     var rows = new Object();
-    DAYS_OF_WEEK.forEach(function(_, d) {
+    DAYS_OF_WEEK.forEach((_, d) => {
         var lessons = new Array();
         for (var i = 0; i < lessons_num; i++) {
             var lesson = {root: i + 1};
@@ -65,10 +78,10 @@ function getTimetable(number) {
         }
         rows[d] = lessons;
     });
-    promise.then(function(data) {
+    promise.then(data => {
         var tt = data.json();
         timetable.config.columns[0].fillspace = false;
-        tt.forEach(function(form) {
+        tt.forEach(form => {
             var f = [form.form_number.toString(), form.form_letter].join("");
             var f_subj = ["subj", f].join("_")
             var f_room = ["room", f].join("_")
@@ -77,17 +90,16 @@ function getTimetable(number) {
                 header: {text: f, colspan: 2, css: {"text-align": "center"}}
             });
             timetable.config.columns.push({id: f_room, header: "", width: 45});
-            DAYS_OF_WEEK.forEach(function(_, d) {
-                var _lessons = form.lessons.filter(
-                    function(l) { return l.day_of_week === d});
-                if (!_lessons) { return; }
+            DAYS_OF_WEEK.forEach((_, d) => {
+                var _lessons = form.lessons.filter(l => l.day_of_week === d);
+                if (!_lessons) return;
                 for (var i = 0; i < lessons_num; i++) {
                     var _subjects = _lessons.filter(
-                        function(s) { return s.lesson_number === i + 1});
-                    if (!_subjects) { return; }
+                        s => s.lesson_number === i + 1);
+                    if (!_subjects) return;
                     var _subjs = new Array();
                     var _rooms = new Array();
-                    _subjects.forEach(function(s) {
+                    _subjects.forEach(s => {
                         _subjs.push(s.subject);
                         var _room = s.classroom;
                         if (_room) {
@@ -105,15 +117,13 @@ function getTimetable(number) {
             {id: "extra", header: "", fillspace: true});
         timetable.config.columns[0].width = 100;
         timetable.refreshColumns();
-        DAYS_OF_WEEK.forEach(function(day_name, day_number) {
+        DAYS_OF_WEEK.forEach((day_name, day_number) => {
             timetable.add(
                 {root: day_name, $css: {"background-color": "#383838"}});
-            rows[day_number].forEach(function(lesson) {
-                timetable.add(lesson);
-            });
+            rows[day_number].forEach(lesson => timetable.add(lesson));
         });
         var max_width = 0;
-        timetable.eachColumn(function(id) {
+        timetable.eachColumn(id => {
             if (id.includes("subj")) {
                 timetable.adjustColumn(id);
                 var _width = timetable.getColumnConfig(id).width;
@@ -122,7 +132,7 @@ function getTimetable(number) {
                 }
             }
         });
-        timetable.eachColumn(function(id) {
+        timetable.eachColumn(id => {
             if (id.includes("subj")) {
                 timetable.setColumnWidth(id, max_width);
             }
@@ -130,7 +140,7 @@ function getTimetable(number) {
         timetable.adjustRowHeight();
         timetable.refresh();
         timetable.hideOverlay();
-    }).fail(function(err) {
+    }).fail(err => {
         timetable.hideOverlay();
         timetable.showOverlay(gettext("No data"));
         webix.message({
@@ -143,12 +153,12 @@ function getTimetable(number) {
 }
 
 function getForms() {
-    var promise = webix.ajax().get("/main/forms/");
-    promise.then(function(data) {
+    var promise = webix.ajax().get(URL_SCHOOL_FORMS);
+    promise.then(data => {
         var _forms = data.json();
         form_tree.add({id: "form_numbers", value: gettext("Forms")});
         form_tree.add({id: "0", value: gettext("All")}, -1, "form_numbers");
-        _forms.forEach(function(el) {
+        _forms.forEach(el => {
             school_forms.set(el["number"], el["letters"]);
             form_tree.add(
                 {id: el["number"].toString(), value: el["number"].toString()},
@@ -156,7 +166,7 @@ function getForms() {
             );
         });
         form_tree.open("form_numbers");
-    }).fail(function(err) {
+    }).fail(err => {
         webix.message({
             text: gettext("Unable to get list of forms"),
             type: "error",
@@ -166,6 +176,9 @@ function getForms() {
     });
 }
 
+//
+// Start-up
+//
 getForms();
 form_tree.attachEvent("onItemClick", function(id) {
     var _number = parseInt(id);
