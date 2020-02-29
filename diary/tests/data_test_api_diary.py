923bf32c0d4d487ecf3362338493ecdeee6375f5
Kolyat <kks.pub@gmail.com>
2020-03-01 00:48:12 +0300

    Update tests for diary API

--- diary/tests/data_test_api_diary.py
+++ diary/tests/data_test_api_diary.py
@@ -15,6 +15,10 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import fastjsonschema
+from rest_framework import status
+
+from testutils import settings
+from testutils.rndutils import random_str
 
 
 validate = fastjsonschema.compile({
@@ -23,9 +27,11 @@ validate = fastjsonschema.compile({
         'record': {
             'type': 'object',
             'properties': {
-                'date': {'type': 'string', 'format': 'date'},
                 'id': {'type': 'integer'},
-                'lesson_number': {'type': 'integer', 'minimum': 1, 'maximum': 7},
+                'user': {'type': 'string', 'minLength': 1},
+                'date': {'type': 'string', 'format': 'date'},
+                'lesson_number': {'type': 'integer',
+                                  'minimum': 1, 'maximum': 7},
                 'subject': {'type': 'string'},
                 'text': {'type': 'string'},
                 'marks': {'type': 'string'},
@@ -35,13 +41,74 @@ validate = fastjsonschema.compile({
             'required': ['date', 'lesson_number', 'subject', 'text']
         }
     },
-    'oneOf': [
+    'anyOf': [
         {
             'type': 'array',
             'minItems': 7,
             'maxItems': 7,
             'items': {'$ref': '#/definitions/record'}
         },
-        {'$ref': '#/definitions/record'}
+        {'$ref': '#/definitions/record'},
+        {'properties': {'school_form': {'type': 'string'}}},
+        {'properties': {'lesson_number': {'type': 'string'}}},
+        {'properties': {'subject': {'type': 'string'}}},
     ]
 })
+
+credentials = {
+    'school_form_assigned': [
+        settings.USER_STUDENT['username'], settings.USER_STUDENT['password'],
+        status.HTTP_200_OK
+    ],
+    'school_form_not_assigned': [
+        settings.USER_ADMIN['username'], settings.USER_ADMIN['password'],
+        status.HTTP_424_FAILED_DEPENDENCY
+    ]
+}
+
+template = {
+    'lesson_number': 3,
+    'subject': 'Физика',
+    'text': 'Some text here'
+}
+
+positive_cases = {
+    'lesson_3': [{'lesson_number': 3}, status.HTTP_201_CREATED],
+    'lesson_1': [{'lesson_number': 1}, status.HTTP_201_CREATED],
+    'lesson_7': [{'lesson_number': 7}, status.HTTP_201_CREATED],
+    'subject_algebra': [{'subject': 'Алгебра'}, status.HTTP_201_CREATED],
+    'subject_blank': [{'subject': ' '}, status.HTTP_201_CREATED],
+    'text_non_empty': [{'text': 'For testing'}, status.HTTP_201_CREATED],
+    'text_unicode': [{'text': random_str(9000)}, status.HTTP_201_CREATED],
+    'text_empty': [{'text': ''}, status.HTTP_201_CREATED],
+    'no_text': [
+        {'lesson_number': 3, 'subject': 'Физика'},
+        status.HTTP_201_CREATED
+    ]
+}
+
+negative_cases = {
+    'lesson_negative': [{'lesson_number': -1}, status.HTTP_400_BAD_REQUEST],
+    'lesson_0': [{'lesson_number': 0}, status.HTTP_400_BAD_REQUEST],
+    'lesson_8': [{'lesson_number': 8}, status.HTTP_400_BAD_REQUEST],
+    'lesson_100': [{'lesson_number': 100}, status.HTTP_400_BAD_REQUEST],
+    'lesson_float': [{'lesson_number': 3.5}, status.HTTP_400_BAD_REQUEST],
+    'lesson_text': [{'lesson_number': '3'}, status.HTTP_201_CREATED],
+    'lesson_null': [{'lesson_number': None}, status.HTTP_400_BAD_REQUEST],
+    'subject_text': [{'subject': 'text'}, status.HTTP_400_BAD_REQUEST],
+    'subject_null': [{'subject': None}, status.HTTP_400_BAD_REQUEST],
+    'subject_123': [{'subject': 123}, status.HTTP_400_BAD_REQUEST],
+    'text_123': [{'text': 123}, status.HTTP_201_CREATED],
+    'text_null': [{'text': None}, status.HTTP_201_CREATED]
+}
+
+incomplete_payload = {
+    'no_lesson_number': [
+        {'subject': 'Физика', 'text': 'Some text here'},
+        status.HTTP_400_BAD_REQUEST
+    ],
+    'no_subject': [
+        {'lesson_number': 3, 'text': 'Some text here'},
+        status.HTTP_400_BAD_REQUEST
+    ]
+}
--- diary/tests/test_api_diary.py
+++ diary/tests/test_api_diary.py
@@ -16,15 +16,47 @@
 
 import logging
 import json
-from rest_framework import test, status
+import copy
+from rest_framework import test
+import ddt
 import pytest
 
-from testutils import settings
+from testutils import settings, ddtutils
 from . import data_test_api_diary
 
 
+@ddt.ddt
 class TestDiaryApiRetrieve(test.APITestCase):
     """Test diary API - retrieve records
+    """
+    @ddt.data(*ddtutils.prepare(data_test_api_diary.credentials))
+    @ddt.unpack
+    def test_retrieve(self, username, password, code):
+        """Cases with response data
+
+        :param username: user's username
+        :param password: user's password
+        :param code: expected code
+        """
+        self.client.login(username=username, password=password)
+        url = settings.DIARY_PATH + '2020/02/29/'
+        logging.info('Request: GET {}'.format(url))
+        try:
+            response = self.client.get(url)
+            data = json.loads(response.content)
+            logging.info('Response code: {}'.format(response.status_code))
+            logging.info('Response data: {}'.format(data))
+            self.assertEqual(response.status_code, code)
+            self.assertIsNotNone(data_test_api_diary.validate(data))
+        except Exception as e:
+            logging.error(e)
+            self.client.logout()
+            self.fail(e)
+
+
+@ddt.ddt
+class TestDiaryApiWrite(test.APITestCase):
+    """Test timetable API - create/update records
     """
     def setUp(self):
         super().setUp()
@@ -35,18 +67,75 @@ class TestDiaryApiRetrieve(test.APITestCase):
         super().tearDown()
         self.client.logout()
 
-    def test_endpoints(self):
-        """Positive case with response data
+    @ddt.data(*ddtutils.prepare(data_test_api_diary.positive_cases))
+    @ddt.unpack
+    def test_positive_cases(self, payload, code):
+        """Positive create/update cases
+
+        :param payload: additional payload
+        :param code: expected code
         """
-        url = settings.DIARY_PATH + '2020/02/29/'
-        logging.info('Request: GET {}'.format(url))
+        url = settings.DIARY_PATH + '2020/03/01/'
+        data = copy.deepcopy(data_test_api_diary.template)
+        data.update(payload)
+        logging.info('Request: POST {}'.format(url))
+        logging.info('Payload: {}'.format(data))
         try:
-            response = self.client.get(url)
-            data = json.loads(response.content)
+            response = self.client.post(url, data)
+            response_data = json.loads(response.content)
             logging.info('Response code: {}'.format(response.status_code))
-            logging.info('Response data: {}'.format(data))
-            self.assertEqual(response.status_code, status.HTTP_200_OK)
-            self.assertIsNotNone(data_test_api_diary.validate(data))
+            logging.info('Response data: {}'.format(response_data))
+            self.assertEqual(response.status_code, code)
+            self.assertIsNotNone(data_test_api_diary.validate(response_data))
+            self.assertTrue(
+                set(payload.items()).issubset(set(response_data.items()))
+            )
+        except Exception as e:
+            logging.error(e)
+            self.fail(e)
+
+    @ddt.data(*ddtutils.prepare(data_test_api_diary.negative_cases))
+    @ddt.unpack
+    def test_negative_cases(self, payload, code):
+        """Negative create/update cases
+
+        :param payload: additional payload
+        :param code: expected code
+        """
+        url = settings.DIARY_PATH + '2020/03/02/'
+        data = copy.deepcopy(data_test_api_diary.template)
+        data.update(payload)
+        logging.info('Request: POST {}'.format(url))
+        logging.info('Payload: {}'.format(data))
+        try:
+            response = self.client.post(url, data)
+            response_data = json.loads(response.content)
+            logging.info('Response code: {}'.format(response.status_code))
+            logging.info('Response data: {}'.format(response_data))
+            self.assertEqual(response.status_code, code)
+            self.assertIsNotNone(data_test_api_diary.validate(response_data))
+        except Exception as e:
+            logging.error(e)
+            self.fail(e)
+
+    @ddt.data(*ddtutils.prepare(data_test_api_diary.incomplete_payload))
+    @ddt.unpack
+    def test_incomplete_payload(self, payload, code):
+        """Negative tests with incomplete payload
+
+        :param payload: incomplete payload
+        :param code: expected code
+        """
+        url = settings.DIARY_PATH + '2020/03/03/'
+        logging.info('Request: POST {}'.format(url))
+        logging.info('Payload: {}'.format(payload))
+        try:
+            response = self.client.post(url, payload)
+            response_data = json.loads(response.content)
+            logging.info('Response code: {}'.format(response.status_code))
+            logging.info('Response data: {}'.format(response_data))
+            self.assertEqual(response.status_code, code)
+            self.assertIsNotNone(data_test_api_diary.validate(response_data))
         except Exception as e:
             logging.error(e)
             self.fail(e)
