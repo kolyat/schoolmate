f27c49f57ad42c13b952b6ba631459356f0af8e4
Kolyat <kks.pub@gmail.com>
2021-12-14 00:33:59 +0300

    Remove unneeded files

--- static/webix/codebase/skins/dark.js
+++
@@ -1,9 +0,0 @@
-webix.skin.contrast.barHeight = 42;
-webix.skin.contrast.tabbarHeight = 40;
-webix.skin.contrast.rowHeight = 32;
-webix.skin.contrast.listItemHeight = 32;
-webix.skin.contrast.inputHeight = 32;
-webix.skin.contrast.layoutMargin.wide = 10;
-webix.skin.contrast.layoutMargin.space = 10;
-webix.skin.contrast.layoutPadding.space = 10;
-webix.skin.set('contrast');
