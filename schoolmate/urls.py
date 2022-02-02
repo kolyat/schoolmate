a568baddd21604c5fa759344cef1474e4f89fdc6
Kolyat <kks.pub@gmail.com>
2022-02-03 02:17:41 +0300

    Redoc (interactive API documentation)

--- schoolmate/urls.py
+++ schoolmate/urls.py
@@ -18,19 +18,34 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.i18n import i18n_patterns
 from django.views import generic, i18n
+from rest_framework import schemas
 
 from schoolmate import settings
 
 
 urlpatterns = [
     path('', include('school.urls')),
+
     path('profile/', include('account.urls')),
     path('news/', include('news.urls')),
     path('timetable/', include('timetable.urls')),
     path('diary/', include('diary.urls')),
     path('notebook/', include('notebook.urls')),
+
     path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
+
+    path('openapi', schemas.get_schema_view(
+        title='Schoolmate',
+        description='School management system API',
+        version='2.0',
+        urlconf='schoolmate.urls'
+    ), name='openapi-schema'),
+    path('redoc/', generic.TemplateView.as_view(
+            template_name='redoc.html.j2',
+            extra_context={'schema_url': 'openapi-schema'}
+        ), name='redoc'),
+
     path('favicon.ico', generic.RedirectView.as_view(
         url='{}img/favicon.ico'.format(settings.STATIC_URL))),
 ]
